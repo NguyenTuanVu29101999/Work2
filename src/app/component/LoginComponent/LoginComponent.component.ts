2e06aae379b660dec13dbca49c439e634536fea5
NguyenTuanVu29101999 <tuanvunguyen199958@gmail.com>
2021-12-20 14:17:12 +0700

    Login

---
+++ src/app/component/LoginComponent/LoginComponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-LoginComponent',
+  templateUrl: './LoginComponent.component.html',
+  styleUrls: ['./LoginComponent.component.css']
+})
+export class LoginComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
